ec9e4b82f09ab37c6deed1d7e2b446e3cdda023a
Ericky Dias <erickyhenriquesd@gmail.com>
2024-04-15 08:22:35 +0100

    add estilos e component infotooltip

---
+++ src/components/infoTooltip.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const InfoTooltip = ({ isOpen, onClose, isSuccess, message }) => {
+  return (
+    <div className={`modal ${isOpen ? "modal_open" : ""}`}>
+      <div className="modal__content">
+        <button type="button" className="modal__close-icon" onClick={onClose}>
+          <img
+            className="modal__close-icon-img"
+            src="./images/close-icon.png"
+            alt="ìcone para fechar o pop-up"
+          />
+        </button>
+        <img className={`modal__icon ${
+            isSuccess ? "modal__icon_success" : "modal__icon_error"
+          }`}/>
+        <p
+          className={`modal__message ${
+            isSuccess ? "modal__message_success" : "modal__message_error"
+          }`}
+        >
+          {message}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoTooltip;
